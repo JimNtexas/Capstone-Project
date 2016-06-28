1ba7cddfd7fe6d1deee1112d496ef49caec63f1b
JimNtexas <grayraven42@gmail.com>
2016-06-27 23:32:41 -0500

    fixed touch behavior on widget

--- ElectoralCalc/app/src/main/java/com/grayraven/electoralcalc/ElectionWidget.java
+++ ElectoralCalc/app/src/main/java/com/grayraven/electoralcalc/ElectionWidget.java
@@ -35,7 +35,7 @@ public class ElectionWidget extends AppWidgetProvider {
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
             views.setOnClickPendingIntent(R.id.the_widget, pendingIntent);
-
+            appWidgetManager.updateAppWidget(appWidgetId,views);
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
